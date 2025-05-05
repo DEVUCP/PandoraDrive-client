a7d4b1ee8e94bcba715c45b84965c57b3c24eca2
DEVUCP <OMHN05@protonmail.com>
2025-05-05 11:51:53 +0300

    refactor(index): Place App component in index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import RegisterScreen from './Pages/RegisterScreen';
 import reportWebVitals from './reportWebVitals';
+
+import { useState, createContext } from 'react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +15,40 @@ root.render(
   </React.StrictMode>
 );
 
+export const portContext = createContext<{
+  port: string;
+  setPort: (port: string) => void;
+}>({
+  port: "",
+  setPort: (port: string) => {}
+});
+
+export const ipContext = createContext<{
+  ip: string;
+  setIp: (ip: string) => void;
+}>({
+  ip: "",
+  setIp: (ip: string) => {}
+});
+
+function App(){
+  const [port, setPort] = useState<string>("55551");
+  const [ip, setIp] = useState<string>("");
+  // localStorage.clear() // for debugging
+
+  return(
+    <>
+    {/* <h1>{ip}{port}</h1> for debugging*/}
+    <ipContext.Provider value={{ip, setIp}}>
+    <portContext.Provider value={{port, setPort}}>
+      <RegisterScreen />
+    </portContext.Provider>
+    </ipContext.Provider>
+    </>
+  );
+}
+
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
