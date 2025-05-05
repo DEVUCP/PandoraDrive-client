21ca1c5fed409b6fdbddc5394e926dd9e583fe62
TheUCP <153499312+DEVUCP@users.noreply.github.com>
2025-05-05 12:18:17 +0300

    fix(CI): Update `App.test.tsx` to support the previous App refactor

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import RegisterScreen from './Pages/RegisterScreen';
+import { ipContext, portContext } from './index';
 
 test('renders learn react link', () => {
-  render(<RegisterScreen />);
-  const linkElement = screen.getByText(/learn react/i);
+  const mockPortContext = {
+    port: "55551",
+    setPort: jest.fn()
+  };
+
+  const mockIpContext = {
+    ip: "",
+    setIp: jest.fn()
+  };
+
+  render(
+    <ipContext.Provider value={mockIpContext}>
+      <portContext.Provider value={mockPortContext}>
+        <RegisterScreen />
+      </portContext.Provider>
+    </ipContext.Provider>
+  );
+
+  const linkElement = screen.getByText(/Welcome to Pandora Home Drive!/i);
   expect(linkElement).toBeInTheDocument();
 });
