265af64465436976875ef9bd48a4090ab61f25e9
DEVUCP <OMHN05@protonmail.com>
2025-05-07 10:35:38 +0300

    ci: Update `App.test.tsx` back to using App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import RegisterScreen from './Pages/RegisterScreen';
+import App from './App';
 
 test('renders learn react link', () => {
-  render(<RegisterScreen />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<App />);
+  const registerScreen = screen.getByRole('main');
+  expect(registerScreen).toBeInTheDocument();
 });
