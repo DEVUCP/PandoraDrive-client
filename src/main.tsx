f9b85537c2792917a72cef7790dd9770c6fa40fd
Marwan Mostafa <marwanm.dev@gmail.com>
2025-05-14 18:11:17 +0300

    fix: Add template for testing tailwind in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <div className="p-10 bg-gray-100">
+      <h2 className="text-3xl font-bold text-yellow-500">
+        This should be yellow
+      </h2>
+    </div>
+    {/* <App /> */}
   </StrictMode>,
 );
